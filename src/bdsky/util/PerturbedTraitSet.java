49df9afd49e61b0960def5cdf181a9bffa5dd385
Tim Vaughan <timothy.vaughan@bsse.ethz.ch>
2025-08-15 14:46:52 +0200

    Added option for PerturbedTraitSet to load data from other TraitSet.

--- src/bdsky/util/PerturbedTraitSet.java
+++ src/bdsky/util/PerturbedTraitSet.java
@@ -7,6 +7,7 @@ import beast.base.util.Randomizer;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Description("Extension of TraitSet which ensures trait values are unique" +
         " by adding a small random perturbation to each.")
@@ -16,7 +17,28 @@ public class PerturbedTraitSet extends TraitSet {
             "Maximum perturbation to apply to trait values.",
             1e-5);
 
+    public Input<TraitSet> otherTraitSetInput = new Input<>("otherTraitSet",
+            "Trait set from which to take values for this trait set.",
+            Input.Validate.REQUIRED);
+
+    public PerturbedTraitSet() {
+        traitsInput.setRule(Input.Validate.OPTIONAL);
+    }
+
     public void initAndValidate() {
+
+        if (otherTraitSetInput.get() != null) {
+            // Extract values from external trait set.
+
+            TraitSet otherTraitSet = otherTraitSetInput.get();
+
+            String newValue = taxaInput.get().getTaxaNames().stream()
+                    .map(taxonName -> taxonName + "=" + otherTraitSet.getStringValue(taxonName))
+                    .collect(Collectors.joining(","));
+
+            traitsInput.setValue(newValue, this);
+        }
+
         super.initAndValidate();
 
         double epsilon = epsilonInput.get();
